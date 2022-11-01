e0d0b7f0526bab1afc54b905a62da477beffee93
Mariya <anteya@bk.ru>
2022-11-02 02:17:53 +0300

    Создана программа, которая которая задаёт массив из 8 элементов и выводит их на экран

---
+++ HWSem4Task3/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+int[] Array = new int[8];
+
+for (int i = 0; i < 8; i++)
+{
+    Array[i] = new Random().Next(0,9);
+    Console.Write(Array[i]);
+}
+Console.WriteLine();
